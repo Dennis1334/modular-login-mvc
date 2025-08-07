79162ded39c17b7eebb3a37b2a0a6617a4151aea
android <rayane.tahara@etec.sp.gov.br>
2025-08-06 22:18:43 -0300

    Atualizacao dos pacotes e classes MVC

--- app/src/main/java/android/dennis/loginmvc/datamodel/UsuarioDataModel.java
+++ app/src/main/java/android/dennis/loginmvc/datamodel/UsuarioDataModel.java
@@ -1,4 +1,28 @@
 package android.dennis.loginmvc.datamodel;
 
+import android.util.Log;
+
 public class UsuarioDataModel {
+
+    public static final String TABELA = "usuario";
+
+    public static final String ID = "id";
+    public static final String NOME = "nome";
+    public static final String EMAIL = "email";
+    public static final String SENHA = "senha";
+
+    public static final String queryCriarTabela = "";
+
+    public static String criarTabela(){
+        queryCriarTabela += "CREATE TABLE " + TABELA + "(";
+        queryCriarTabela += ID + "int primary key autoincrement";
+        queryCriarTabela += NOME + "text";
+        queryCriarTabela += EMAIL + "texte";
+        queryCriarTabela += SENHA + "texte";
+        queryCriarTabela += ")";
+
+        Log.i("tabela", "criarTabela: " + queryCriarTabela);
+        return queryCriarTabela;
+
+    }
 }
